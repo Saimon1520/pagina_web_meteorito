0c168a696287679b6a6e19a605f4b6eb0821a861
guerreroandrey5 <guerreroandrey@gmail.com>
2023-10-30 17:32:29 -0600

    Initial design started

---
+++ src/components/AZPage/AZPage.js
@@ -0,0 +1,22 @@
+import "./AZPage.css";
+import React from "react";
+
+const AZPage = () => {
+  return (
+    <>
+      <div className="photo">
+        <h1>Aguas Zarcas photo</h1>
+      </div>
+      <div className="POIs">
+      <li>Colegio</li>
+      <li>Iglesia</li>
+      <li>Rios</li>
+      <li>Volcanes</li>
+      </div>
+      <div className="history">
+        <h1>Aguas Zarcas history</h1>
+        </div>
+    </>
+  );
+};
+export default AZPage;
