f2fe5e7a939f26960fdd7d28aa689264a8d4e680
LSCAR23 <laraoscar2310@gmail.com>
2023-11-04 16:36:54 -0600

    responsive footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,12 +4,17 @@ const Footer = () => {
     return (
         <footer className="footer-container">
             <div className="footer-content">
-                <p>&copy; 2023 CODEIN</p>
-                <ul>
-                    <li><a href="https://www.facebook.com"><FaFacebook /></a></li>
-                    <li><a href="https://cr.linkedin.com"><FaLinkedin /></a></li>   
-                    <li><a href="https://www.instagram.com/codein_vr/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA=="><FaInstagram /></a></li>
-                </ul>
+                <div className='copy_content'>
+                    <p>&copy; 2023 CODEIN</p>
+                </div>
+                <div className='social_content'>
+                    <ul>
+                        <li><a href="https://www.facebook.com"><FaFacebook /></a></li>
+                        <li><a href="https://cr.linkedin.com"><FaLinkedin /></a></li>
+                        <li><a href="https://www.instagram.com/codein_vr/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA=="><FaInstagram /></a></li>
+                    </ul>
+                </div>
+
             </div>
         </footer>
     );
