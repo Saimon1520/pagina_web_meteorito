4c9b52c9d88c8053a9b6111bf0827203c8960a53
DaveCortes2003 <deivyduo@gmail.com>
2023-11-03 22:31:22 -0600

    f

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         </Router>
       </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
