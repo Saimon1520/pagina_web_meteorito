1a3b219e8e3b4ddb404a35fba398c2179e4531d1
LSCAR23 <laraoscar2310@gmail.com>
2023-11-04 15:35:22 -0600

    aesthetic fixes

--- src/components/Meteoritos/Meteoritos.js
+++ src/components/Meteoritos/Meteoritos.js
@@ -13,7 +13,7 @@ const Meteoritos = () => {
                 </div>
                 <div className='definition_container'>
                     <div className='text_container'>
-                        <p className='text_definition'>
+                        <p className='info_P'>
                             La Real Academia Española lo define como: “Fragmento de un cuerpo celeste que cae sobre la Tierra, o
                             sobre un astro cualquiera”. Si bien es cierto, observar la caída de fragmentos de cuerpos celestes a la tierra es para
                             el ser humano un hecho hermoso de admirar, sin embargo, no es un acontecimiento poco cotidiano, según Lücke, Madrigal y
@@ -37,10 +37,16 @@ const Meteoritos = () => {
                     <h2>Se pueden citar algunos aspectos que hacen particularmente importante el meteorito Aguas Zarcas:</h2>
                 </div>
                 <div className='text_info_container'>
-                    <p>
+                    <p className='info_P'>
                         1.	Es muy poco probable que un meteorito sea bautizado con el nombre del lugar donde cayó, a través de la historia únicamente se han registrado 1196 meteoritos que toman el nombre del lugar.
+                    </p>
+                    <br></br>
+                    <p className='info_P'>
                         2.	La mayoría de los meteoritos que aterrizan en la tierra son trozos de piedra o metal, en el caso del meteorito Aguas Zarcas es una condrita carbonácea, un remanente  del Sistema Solar primitivo.  Esta particularidad es la que le brinda el valor histórico – científico al Meteorito Aguas Zarcas, según la astrofísica Carolina Salas Matamoros, investigadora del Centro de Investigaciones Espaciales (Cinespa) de la UCR, explica que el meteorito de Aguas Zarcas es de suma  importancia para el estudio sobre la formación del sistema solar en su etapa inicial, e indica que, este meteorito está compuesto por moléculas orgánicas, como los aminoácidos, que son la base de las proteínas y desempeñan un papel clave en casi todos los procesos biológicos.
-                        3.	Solo se han registrado dos meteoritos a nivel mundial con estas características, en 1969 cayó en Murchison, un pueblo ganadero australiano, un meteorito con estas mismas características, de los cuales se recogieron alrededor de 100 kilogramos de este. Y en el 2019 en el distrito de Aguas Zarcas, donde se recolectaron aproximadamente 30 kilogramos. Según investigadores hace más de cincuenta años que cayó el meteorito Murchison, no se tenía la tecnología con la que se cuenta ahora para realizar las pruebas e investigación como con el Meteorito Aguas Zarcas, razón por la cual aumenta el valor de este.
+                    </p>
+                    <br></br>
+                    <p className='info_P'>                       
+                         3.	Solo se han registrado dos meteoritos a nivel mundial con estas características, en 1969 cayó en Murchison, un pueblo ganadero australiano, un meteorito con estas mismas características, de los cuales se recogieron alrededor de 100 kilogramos de este. Y en el 2019 en el distrito de Aguas Zarcas, donde se recolectaron aproximadamente 30 kilogramos. Según investigadores hace más de cincuenta años que cayó el meteorito Murchison, no se tenía la tecnología con la que se cuenta ahora para realizar las pruebas e investigación como con el Meteorito Aguas Zarcas, razón por la cual aumenta el valor de este.
                     </p>
                 </div>
             </div>
